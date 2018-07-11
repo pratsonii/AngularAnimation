d82836b3dd98ff6615c36662f5a388ad6648d0f0
DESKTOP-CK0M5E4\pratik <soniipratik@gmail.com>
2018-07-11 23:37:46 +0530

    changes for animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { trigger, style, transition, animate, group } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+  trigger('flyInOut', [
+    transition(':enter', [
+      style({transform: 'translateX(-100%)'}),
+      animate(350)
+    ]),
+    transition(':leave', [
+      animate(350, style({transform:  'translateX(-100%)'}))
+    ])
+  ]),
+
+  trigger('flyOutIn', [
+    transition(':enter', [
+      style({transform: 'translateX(100%)'}),
+      animate(350)
+    ]),
+    transition(':leave', [
+      animate(350, style({transform:  'translateX(100%)'}))
+    ])
+  ])
+]
 })
 export class AppComponent {
   title = 'app';
+  show : boolean = true;
+
+  toggle()
+  {
+    this.show = this.show ? false : true;
+  }
 }
